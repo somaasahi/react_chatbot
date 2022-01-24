04af31f2832fd8540755c45865d43b44c0f2d9e2
soma_asahi <95704277+somaasahi@users.noreply.github.com>
2022-01-25 00:45:00 +0900

    fix

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -2,20 +2,21 @@ import React from "react";
 import ListItem from '@mui/material/ListItem';
 import ListItemAvatar from '@mui/material/ListItemAvatar';
 import Avatar from '@mui/material/Avatar';
-import St from '../assets/img/st.jpeg';
+import St from '../assets/img/mei.jpeg';
 import Ga from '../assets/img/ga.jpeg';
 
+
 const Chat = (props) => {
-  const isQuestion = (props.type === 'quetion');
-  const classes = isQuestion ? 'p-chat__row' : 'p-chat__reverse';
+  const isQuestion = (props.type === 'question');
+  const classes = isQuestion ? "p-chat__row" : "p-chat__reverse";
 
   return (
-    <ListItem className="{classes}">
+    <ListItem className={classes}>
         <ListItemAvatar>
           {isQuestion ? (
-            <Avatar alt="icon" src={St} />
-          ) : (
             <Avatar alt="icon" src={Ga} />
+          ) : (
+            <Avatar alt="icon" src={St} />
           )}
           
         </ListItemAvatar>
