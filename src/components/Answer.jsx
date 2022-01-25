d41a7e10ce1107221dd8dc8ff182c6f68e2160e8
soma_asahi <95704277+somaasahi@users.noreply.github.com>
2022-01-25 23:08:41 +0900

    materialUIcss

--- src/components/Answer.jsx
+++ src/components/Answer.jsx
@@ -1,12 +1,29 @@
 import React from "react";
 import Button from '@mui/material/Button';
-import Stack from '@mui/material/Stack';
+import { makeStyles } from '@material-ui/styles';
 
+
+const useStyles = makeStyles({
+  button: {
+    borderColor: '#FFB549',
+    color: '#FFB549',
+    fontweight: 600,
+    marginBottom: '8px',
+    "&:hover": {
+      borderColor: '#FFB549',
+      backgroundColor: '#FFB549',
+      color: '#fff'
+    }
+  },
+})
 
 const Answer = (props) => {
+  const classes = useStyles()
 
   return(
-    <Button variant="contained" onClick={() => props.select(props.content, props.nextId)}>
+    <Button 
+    className={classes.button}
+    variant="outlined" onClick={() => props.select(props.content, props.nextId)}>
       {props.content}
     </Button>
   )
--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -1,13 +1,22 @@
 import * as React from 'react';
 import List from '@mui/material/List';
 import {Chat} from './index';
+import { makeStyles } from '@material-ui/styles';
 
+const useStyles = makeStyles({
+  chats: {
+    height: 400,
+    padding: '0',
+    overflow: 'auto'
+  }
+})
 
 
 const Chats = (props) => {
+  const classes = useStyles()
 
   return (
-    <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+    <List className={classes.chats}>
       {props.chats.map((chat, index) => {
         return <Chat text={chat.text} type={chat.type} key={index.toString()}/>
       })}
